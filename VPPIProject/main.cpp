2b0bbff67d0f0265a5f3b5ee7af051435548a461
Fourier2718281828 <non-linear@ukr.net>
2023-01-29 05:24:28 +0200

    +notes

--- VPPIProject/main.cpp
+++ VPPIProject/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <vector>
+#include "Notes.h"
+#include <Windows.h>
 using std::cout;
 
 int main()
 {
+	std::vector<INote*> notes
+	{
+		new NoteA(1),
+		new NoteASharp(1),
+		new NoteB(1),
+		new NoteC(1),
+		new NoteCSharp(1),
+	};
+
+	for (const auto* note : notes)
+	{
+		Beep(note->get_frequency(), 500);
+	}
+
 	return 0;
 }
